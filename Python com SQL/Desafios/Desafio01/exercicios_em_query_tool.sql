cca64189785cbfbda59da4c0276d7470b7a42deb
Henrique Patricio de Souza da Silva <henriquepworking@gmail.com>
2024-11-29 17:25:33 -0300

    feat: add new content about SQL - Introduction

---
+++ Python com SQL/Desafios/Desafio01/exercicios_em_query_tool.sql
@@ -0,0 +1,33 @@
+-- Primeiro Questionário 
+-- 1
+
+--SELECT * FROM "customers"
+
+
+-- 2
+
+--SELECT "CustomerName", "City" FROM "customers"
+
+-- 3
+
+-- SELECT * FROM "customers"
+-- WHERE "Country" = 'Germany'
+
+-- 4 
+-- SELECT * FROM "customers"
+-- WHERE "CustomerID" <= 5
+
+-- 5
+
+-- SELECT * FROM "customers"
+-- WHERE "City" = 'Berlin' and "Country" = 'Germany'
+
+-- 6 
+
+-- SELECT COUNT("CustomerName"), "CustomerName" FROM "customers"
+-- GROUP BY "CustomerName"
+
+-- 7
+
+-- SELECT * FROM "customers"
+-- LIMIT 5
